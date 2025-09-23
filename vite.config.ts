7951ecd03b0068fc6c245b177e54f5a0bdfda674
Marcus Paixão <marcuspaixao0@gmail.com>
2025-09-23 18:04:23 -0300

    [dyad] add Dyad component tagger

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import dotenv from 'dotenv'
+import dyadComponentTagger from '@dyad-sh/react-vite-component-tagger';
 
 // Carrega as variáveis de ambiente do arquivo .env
 dotenv.config()
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [dyadComponentTagger(), react()],
   server: {
     proxy: {
       '/api/asaas': {
