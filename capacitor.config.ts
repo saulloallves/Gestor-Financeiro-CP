f6b7b97988615890e84c3cd8fdea98e1dcd62bd4
Marcus Paixão <marcuspaixao0@gmail.com>
2025-09-30 11:20:23 -0300

    remove: exclui arquivo de configuração do Capacitor

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.gfcp',
-  appName: 'gf-cp',
-  webDir: 'dist'
-};
-
-export default config;
